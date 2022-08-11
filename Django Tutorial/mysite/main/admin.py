96b57bac7d13ad8b70fcb9f9dc311dec0f94b15f
OatsProgramming <107077453+OatsProgramming@users.noreply.github.com>
2022-08-11 01:39:44 -0500

    Admin Dashboard
    Most of the programming was in terminal
    Created a Django account
    Imported the models' objects into the dashboard

--- Django Tutorial/mysite/main/admin.py
+++ Django Tutorial/mysite/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import ToDoList, Items
 # Register your models here.
+
+admin.site.register(ToDoList) # This is to import the data into the admin dashboard 
+admin.site.register(Items)
