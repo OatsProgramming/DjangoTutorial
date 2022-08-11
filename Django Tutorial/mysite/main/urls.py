ca0ab346ca23733861de67786a7509b437396a81
OatsProgramming <107077453+OatsProgramming@users.noreply.github.com>
2022-08-11 05:06:27 -0500

    Templates and Custom HTML
    Created a base HTML template
    Home HTML file for home page
    List HTML file to show the ToDoList title and its items
    - Used a for loop to iterate thru the items
    
    Messed with the views.py to render the list html file and home html file
    
    Created a home path in urls.py(main)

--- Django Tutorial/mysite/main/urls.py
+++ Django Tutorial/mysite/main/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views # Pretty much import views from the current directory
 
 urlpatterns = [
-path("<int:id>", views.index, name="index") # With <int:id>, were going to look for some integer in our path and pass it to the function used in views.index
+path("<int:id>", views.index, name="index"), # With <int:id>, were going to look for some integer in our path and pass it to the function used in views.index
 # Now, when running this on the website, it would give an error unless you do http://127.0.0.1:8000/"ENTER ANY NUMBER HERE"
 # Once you do, it would show any number that you input in a different page
+
+path('', views.home, name='home') # Since its an empty string, this will automatically direct to views.py function home (check views.py to continue)
 ]
--- Django Tutorial/mysite/main/views.py
+++ Django Tutorial/mysite/main/views.py
@@ -7,6 +7,8 @@ from .models import ToDoList, Items
 
 def index(response, id):
     ls = ToDoList.objects.get(id = id)
-    items = ls.items_set.get(id = 1)
-    return HttpResponse(f'<h1>{ls.name}</h1><br></br><p>{items.text}</p>')
 
+    return render (response, "main/list.html", {"ls":ls})
+
+def home(response):
+    return render (response, "main/home.html", {}) # After setting the path, it renders "main/home.html" which uses the base.html as extension (check home.html file)
