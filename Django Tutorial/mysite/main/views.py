470ae75df5e9958607f659bee0d7a601b97ea5cb
OatsProgramming <107077453+OatsProgramming@users.noreply.github.com>
2022-08-11 22:21:41 -0500

    Created a custom form

--- Django Tutorial/mysite/main/views.py
+++ Django Tutorial/mysite/main/views.py
@@ -9,6 +9,24 @@ from .forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id = id)
+    
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.items_set.all():
+                if response.POST.get(c + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                item.save()
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 2:
+                ls.items_set.create(text = txt, complete = False)
+            else:
+                print("invalid")
+
 
     return render (response, "main/list.html", {"ls":ls})
 
@@ -22,7 +40,7 @@ def create(response):
             n = form.cleaned_data["name"]
             t = ToDoList(name = n )
             t.save()
-        return HttpResponseRedirect("/%i" %t.id)
+        return HttpResponseRedirect(f"/{t.id}")
     else:
         form = CreateNewList()
     return render (response, "main/create.html", {"form": form})
