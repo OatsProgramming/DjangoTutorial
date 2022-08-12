456545f39ee912e546f835acecf6d79c1bde6bb6
OatsProgramming <107077453+OatsProgramming@users.noreply.github.com>
2022-08-11 21:59:48 -0500

    Creating simple forms

--- Django Tutorial/mysite/main/views.py
+++ Django Tutorial/mysite/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
 from .models import ToDoList, Items
+from .forms import CreateNewList
 # Create your views here.
 
 # Create a function that represent the view
@@ -12,3 +14,16 @@ def index(response, id):
 
 def home(response):
     return render (response, "main/home.html", {}) # After setting the path, it renders "main/home.html" which uses the base.html as extension (check home.html file)
+
+def create(response):
+    if response == "POST":
+        form = CreateNewList(response.POST) # All the info from the form 
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name = n )
+            t.save()
+        return HttpResponseRedirect("/%i" %t.id)
+    else:
+        form = CreateNewList()
+    return render (response, "main/create.html", {"form": form})
+
